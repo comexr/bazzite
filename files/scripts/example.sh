95143161afcd0cf0392314d539fae6265db98ced
COMEXR B.V. <64539804+comexr@users.noreply.github.com>
2026-05-22 10:47:18 +0200

    Rebase to bazzite-dx and add Brave

--- files/scripts/example.sh
+++ files/scripts/example.sh
@@ -8,3 +8,4 @@ set -oue pipefail
 # Your code goes here.
 echo 'This is an example shell script'
 echo 'Scripts here will run during build if specified in recipe.yml'
+curl -fsS https://dl.brave.com/install.sh | FLAVOR=origin CHANNEL=beta sh
